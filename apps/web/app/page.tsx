a365ffdcca4ef56afed8c479d4db6d5fb84f173c
ser-emejia <emanuelxmejia@icloud.com>
2026-05-23 15:43:36 -0700

    feat(web) update app.tsx text to Hola Mundo updated 1

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
         >
           Descubre a Yomif Kejelcha
         </a>
-        <p>Hola mundo!</p>
+        <p>Hola mundo! Updated 1</p>
         <Button appName="web">Open alert</Button>
       </main>
       <footer className={styles.footer}>
